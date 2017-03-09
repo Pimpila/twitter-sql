8b0270be12809128658fb5f688d13c8f7819e2e2
Pimpila <pimpila@gmail.com>
2017-03-09 17:28:31 -0500

    up to post but filtering not through sql

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var client = require('../db');
 // a reusable function
 function respondWithAllTweets (req, res, next){
 
-  client.query('SELECT name, content FROM tweets, users WHERE tweets.user_id = users.id', function (err, result) {
+  client.query('SELECT name, content, tweets.id FROM tweets, users WHERE tweets.user_id = users.id', function (err, result) {
     if (err) return next(err); // pass errors to Express
     var tweets = result.rows;
     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -21,44 +21,63 @@ router.get('/tweets', respondWithAllTweets);
 
 // single-user page
 router.get('/users/:username', function(req, res, next){
-  var username = req.params.username;
-
-  client.query('SELECT name, content FROM tweets, users WHERE tweets.user_id = users.id', function (err, result) {
+  var username = req.params.username
+   client.query('SELECT name, content FROM tweets, users HAVING users.name = "' + username + '" ', function (err, result) {
     if (err) return next(err); // pass errors to Express
     var tweets = result.rows;
-    var userTweets = []
-    tweets.forEach(function(obj){
-      if(obj.name === username){
-        userTweets.push(obj)
-      }
-
+    console.log(tweets);
+    // var userTweets = []
+    // tweets.forEach(function(obj){
+    //   if(obj.name === username){
+    //     userTweets.push(obj)
+    //   }
       // now userTweets is all of usernames tweets
-    })
-    res.render('index', { title: 'Twitter.js', tweets: userTweets, showForm: true });
+    // });
+    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
   });
-
-  // var tweetsForName = tweetBank.find({ name: req.params.username });
-  // res.render('index', {
-  //   title: 'Twitter.js',
-  //   tweets: tweetsForName,
-  //   showForm: true,
-  //   username: req.params.username
-  // });
 });
+  // client.query('SELECT name, content FROM tweets, users WHERE tweets.user_id = users.id', function (err, result) {
+  //   if (err) return next(err); // pass errors to Express
+  //   var tweets = result.rows;
+  //   var userTweets = []
+  //   tweets.forEach(function(obj){
+  //     if(obj.name === username){
+  //       userTweets.push(obj)
+  //     }
+  //     // now userTweets is all of usernames tweets
+  //   });
+  //   res.render('index', { title: 'Twitter.js', tweets: userTweets, showForm: true });
+  // });
+// });
 
 // single-tweet page
 router.get('/tweets/:id', function(req, res, next){
-  var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
-  res.render('index', {
-    title: 'Twitter.js',
-    tweets: tweetsWithThatId // an array of only one element ;-)
+  var id = Number(req.params.id);
+
+  client.query('SELECT * FROM tweets', function (err, result) {
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    var tweetByID = [];
+    tweets.forEach(function(obj){
+      if(obj.id === id){
+        tweetByID.push(obj);
+      }
+    });
+      res.render('index', { title: 'Twitter.js', tweets: tweetByID, showForm: true });
   });
 });
 
 // create a new tweet
 router.post('/tweets', function(req, res, next){
-  tweetBank.add(req.body.name, req.body.text);
-  res.redirect('/');
+  client.query('SELECT id  FROM users WHERE name = req.body.name', function (err, result) {
+    if(err) return next(err);
+    // if (result), INSERT the tweet under exisitng id
+    //if(req.body.name === )
+    // else: INSERT new user and new gtweet
+
+  // tweetBank.add(req.body.name, req.body.text);
+  // res.redirect('/');
+  })
 });
 
 // // replaced this hard-coded route with general static routing in app.js
