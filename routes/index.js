a8ea47097b6adba436932f3578666bd224683527
Pimpila <pimpila@gmail.com>
2017-03-09 18:07:57 -0500

    code up to review session

--- routes/index.js
+++ routes/index.js
@@ -22,55 +22,53 @@ router.get('/tweets', respondWithAllTweets);
 // single-user page
 router.get('/users/:username', function(req, res, next){
   var username = req.params.username
-   client.query('SELECT name, content FROM tweets, users HAVING users.name = "' + username + '" ', function (err, result) {
+   client.query('SELECT name, content FROM tweets, users WHERE users.id = tweets.id AND users.name = $1', [username], function (err, result) {
     if (err) return next(err); // pass errors to Express
     var tweets = result.rows;
-    console.log(tweets);
-    // var userTweets = []
-    // tweets.forEach(function(obj){
-    //   if(obj.name === username){
-    //     userTweets.push(obj)
-    //   }
-      // now userTweets is all of usernames tweets
-    // });
     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
   });
 });
-  // client.query('SELECT name, content FROM tweets, users WHERE tweets.user_id = users.id', function (err, result) {
-  //   if (err) return next(err); // pass errors to Express
-  //   var tweets = result.rows;
-  //   var userTweets = []
-  //   tweets.forEach(function(obj){
-  //     if(obj.name === username){
-  //       userTweets.push(obj)
-  //     }
-  //     // now userTweets is all of usernames tweets
-  //   });
-  //   res.render('index', { title: 'Twitter.js', tweets: userTweets, showForm: true });
-  // });
-// });
+
 
 // single-tweet page
 router.get('/tweets/:id', function(req, res, next){
   var id = Number(req.params.id);
 
-  client.query('SELECT * FROM tweets', function (err, result) {
-    if (err) return next(err); // pass errors to Express
+  client.query('SELECT * FROM tweets WHERE id = $1', [id], function(err, result){
+    if(err) return next(err);
     var tweets = result.rows;
-    var tweetByID = [];
-    tweets.forEach(function(obj){
-      if(obj.id === id){
-        tweetByID.push(obj);
-      }
-    });
-      res.render('index', { title: 'Twitter.js', tweets: tweetByID, showForm: true });
+    res.render('index',  {title: 'Twitter.js', tweets: tweets, showForm: true });
   });
+
 });
 
 // create a new tweet
 router.post('/tweets', function(req, res, next){
-  client.query('SELECT id  FROM users WHERE name = req.body.name', function (err, result) {
+  var tweeter = req.body.name;
+  var text = req.body.text;
+
+  client.query('SELECT id FROM users WHERE name = $1,' [req.body.name], function (err, result) {
     if(err) return next(err);
+    var tweets = result.rows;
+    if(tweets.length){
+      // var tweet = result.rows;
+      // post tweet under id
+      client.query('INSERT into tweets (user_id, content) VALUES $1, $2', [tweet.id, text], function(err, result){
+        if(err) return next(err);
+        console.log('user tweeted again', result);
+      });
+    }else{
+      client.query('INSERT into users (name) VALUES $1', [tweeter], function(err, result){
+        if(err) return next(err);
+        console.log('new user tweeted', result);
+      });
+      client.query('INSERT into tweets (user_id, content) VALUE $1 $2', [users.id, text], function(err, result){
+        if(err) return next(err);
+        console.log('new user tweeted', result);
+      });
+      client.end();
+      res.redirect('/');
+    }
     // if (result), INSERT the tweet under exisitng id
     //if(req.body.name === )
     // else: INSERT new user and new gtweet
